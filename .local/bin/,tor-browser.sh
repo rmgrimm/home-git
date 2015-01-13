52bc9787a8ba2fe9806236b16590ca2ea1b52f7d
Robert Grimm <grimm.rob@gmail.com>
2015-01-12 20:55:22 -0600

    Add missing " in ,tor-browser.sh

--- .local/bin/,tor-browser.sh
+++ .local/bin/,tor-browser.sh
@@ -4,4 +4,4 @@ if [ ! -d "$HOME/.local/tor-browser" ]; then
     "$HOME/.local/nonpath-scripts/install-tor-browser.sh"
 fi
 
-exec "$HOME/.local/tor-browser/start-tor-browser "$@"
+exec "$HOME/.local/tor-browser/start-tor-browser" "$@"
