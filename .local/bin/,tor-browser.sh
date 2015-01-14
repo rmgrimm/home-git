7adc7ca4830746417212b2b280102e5def0442c9
Robert Grimm <grimm.rob@gmail.com>
2015-01-13 23:43:38 -0600

    Add htoprc; Fix up tor browser install/run scripts

--- .local/bin/,tor-browser.sh
+++ .local/bin/,tor-browser.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 if [ ! -d "$HOME/.local/tor-browser" ]; then
-    "$HOME/.local/nonpath-scripts/install-tor-browser.sh"
+    if ! "$HOME/.local/nonpath-scripts/install-tor-browser.sh"; then
+        echo
+        echo "failed to install tor browser; exiting..."
+        exit 1
+    fi
 fi
 
 exec "$HOME/.local/tor-browser/start-tor-browser" "$@"
--- .local/nonpath-scripts/install-tor-browser.sh
+++ .local/nonpath-scripts/install-tor-browser.sh
@@ -8,30 +8,51 @@ case $(uname -i) in
     *) echo "unknown arch; aborting install..."; exit 1
 esac
 
-TOR_BROWSER_VERSION=$(
-    curl "https://www.torproject.org/projects/torbrowser.html.en#downloads" \
-    | grep --only-matching --extended-regexp \
-        --regexp=">\\($VERSION_REGEXP\\)<" \
-    | head --lines=1)
-TOR_BROWSER_VERSION=${TOR_BROWSER_VERSION:2:-2}
+if [ x"$1" = x"" ]; then
+    TOR_BROWSER_VERSION=$(
+        curl --silent \
+            "https://www.torproject.org/projects/torbrowser/RecommendedTBBVersions" \
+            | grep --only-matching --extended-regexp \
+            --regexp="\".+\\-Linux\"," \
+            | head --lines=1)
+    TOR_BROWSER_VERSION=${TOR_BROWSER_VERSION:1:-8}
+else
+    TOR_BROWSER_VERSION="$1"
+fi
 
-# TODO(rgrimm): Get architecture (32/64)
+echo "Installing Tor Browser bundle v${TOR_BROWSER_VERSION}..."
+
 TOR_BROWSER_FILE="tor-browser-${TOR_BROWSER_ARCH}-${TOR_BROWSER_VERSION}_en-US.tar.xz"
 TOR_BROWSER_URL="https://www.torproject.org/dist/torbrowser/${TOR_BROWSER_VERSION}/${TOR_BROWSER_FILE}"
 
-mkdir -p "$HOME/Downloads" "$HOME/.local/tor-browser"
+mkdir -p "$HOME/Downloads"
 rm -f "$HOME/Downloads/${TOR_BROWSER_FILE}"
 rm -f "$HOME/Downloads/${TOR_BROWSER_FILE}.asc"
-wget "${TOR_BROWSER_URL}" -O"$HOME/Downloads/${TOR_BROWSER_FILE}"
-wget "${TOR_BROWSER_URL}.asc" -O"$HOME/Downloads/${TOR_BROWSER_FILE}.asc"
 
-gpg --keyserver x-hkp://pool.sks-keyservers.net --recv-keys 0x416F061063FEE659
+echo "  Downloading ${TOR_BROWSER_FILE}..."
+wget --quiet "${TOR_BROWSER_URL}" \
+    --output-document="$HOME/Downloads/${TOR_BROWSER_FILE}"
+wget --quiet "${TOR_BROWSER_URL}.asc" \
+    --output-document="$HOME/Downloads/${TOR_BROWSER_FILE}.asc"
+echo "  Downloading ${TOR_BROWSER_FILE}...done."
+
+echo "  Downloading key and verifying ${TOR_BROWSER_FILE}..."
+gpg --keyserver x-hkp://pool.sks-keyservers.net \
+    --recv-keys 0x416F061063FEE659 \
+    || (echo "download failed; aborting..."; false) || exit 1
 gpg --verify "$HOME/Downloads/${TOR_BROWSER_FILE}.asc" \
-    "$HOME/Downloads/${TOR_BROWSER_FILE}" || exit 1
+    "$HOME/Downloads/${TOR_BROWSER_FILE}" \
+    || (echo "failed; aborting..."; false) || exit 1
+echo "  Downloading key and verifying ${TOR_BROWSER_FILE}...done."
 
+echo "  Extracting ${TOR_BROWSER_FILE}..."
+mkdir -p "$HOME/.local/tor-browser"
 tar --extract --xz --strip-components=1 \
     --directory "$HOME/.local/tor-browser/" \
     --file "$HOME/Downloads/${TOR_BROWSER_FILE}"
+echo "  Extracting ${TOR_BROWSER_FILE}...done."
 
 rm -f "$HOME/Downloads/${TOR_BROWSER_FILE}"
 rm -f "$HOME/Downloads/${TOR_BROWSER_FILE}.asc"
+
+echo "Installing Tor Browser bundle v${TOR_BROWSER_VERSION}...done."
