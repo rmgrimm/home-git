593615da60dc2989966e3bec41a3c556324dd9f1
Robert Grimm <grimm.rob@gmail.com>
2016-07-27 00:15:21 -0500

    Build some docker support functions

--- .local/bin/docker/.docker-init.sh
+++ .local/bin/docker/.docker-init.sh
@@ -3,12 +3,36 @@
 # Ideas borrowed from:
 # https://github.com/jfrazelle/dotfiles/blob/fdff32b013f55bf4e78dbd89f5f2660f53aec687/.dockerfunc
 
-___d_cleanup() {
-  docker rm $(docker ps -aq 2>/dev/null) 2>/dev/null
-  docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
-  docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
+__rmg_d_cleanup() {
+    docker rm $(docker ps -aq 2>/dev/null) 2>/dev/null
+    docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
+    docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
 }
-alias ,d-cleanup="___d_cleanup"
+alias ,d-cleanup="__rmg_d_cleanup"
+
+__rmg_d_run() {
+    case "$(uname -m)" in
+        x86) DOCKER_IMAGE=$1;;
+        x86_64) DOCKER_IMAGE=$2;;
+        armv7l) DOCKER_IMAGE=$3;;
+    esac
+
+    if [ -z "$DOCKER_IMAGE" -o "$DOCKER_IMAGE" = "-" ]; then
+        return 1
+    fi
+
+    shift 3
+
+    DOCKER_NAME=$(echo $DOCKER_IMAGE | cut -d/ -f2)
+
+    docker run "$@" --name $DOCKER_NAME $DOCKER_IMAGE
+}
+__rmg_d_run_cli() {
+    DOCKER_IMAGE=$1
+    shift
+    __rmg_d_run "$DOCKER_IMAGE" "$DOCKER_IMAGE" "$DOCKER_IMAGE" "$@"
+}
+alias ,d-run="__rmg_d_run_cli"
 
 # If running interactively, message the user that docker is ready
 case $- in
