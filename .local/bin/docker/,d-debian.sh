2d807ed0ad3bbf77ff5a58bf5fa9baedd252b920
Robert Grimm <grimm.rob@gmail.com>
2016-09-02 22:52:28 -0500

    Add dockerized-firefox desktop entry
    Also a few updates related to ssh-agent starting with sshag

--- .local/bin/docker/,d-debian.sh
+++ .local/bin/docker/,d-debian.sh
@@ -7,7 +7,7 @@
 
 # Args: <x86 image> <x86_64 image> <armv7l image> [[docker args] \
 #   -- [command [args]]]
-__rmg_d_run debian debian armv7/armhf-debian \
+__rmg_d_run debian debian rmgrimm/armhf-debian \
             --rmg-multi \
             --user root:root \
             -it \
