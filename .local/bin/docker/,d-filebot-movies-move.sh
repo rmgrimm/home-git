550bd67efdaea861d968d08ababb2d96f5bab133
Robert Grimm <grimm.rob@gmail.com>
2017-03-07 02:55:18 -0600

    Dockerized filebot scripts

---
+++ .local/bin/docker/,d-filebot-movies-move.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+. .docker-init.sh
+
+if [ "$#" -gt 0 ]; then
+  DATA=$1
+  shift
+else
+  echo >&2 "Usage: `basename $0` <data directory> [media directory] [filebot options]"
+  exit 1
+fi
+
+if [ -d "$1" ]; then
+  VOLUME=$1
+  shift
+else
+  VOLUME=$PWD
+fi
+
+__rmg_d_run rednoah/filebot rednoah/filebot - \
+            --rmg-multi \
+            -v "${VOLUME}:/volume1" \
+            -v "${DATA}:/data" \
+            -- \
+            -rename \
+            --db TheMovieDB \
+            --action move \
+            --conflict skip \
+            --format "/volume1/filebot-output/{n.colon(' - ')} ({y})/{n.colon(' - ')} ({y}){' - part'+pi}{'.'+lang}" \
+            -non-strict \
+            -r \
+            /volume1 \
+            "$@"
---
+++ .local/bin/docker/,d-filebot-tvshows-test.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+. .docker-init.sh
+
+if [ "$#" -gt 0 ]; then
+  DATA=$1
+  shift
+else
+  echo >&2 "Usage: `basename $0` <data directory> [media directory] [filebot options]"
+  exit 1
+fi
+
+if [ -d "$1" ]; then
+  VOLUME=$1
+  shift
+else
+  VOLUME=$PWD
+fi
+
+__rmg_d_run rednoah/filebot rednoah/filebot - \
+            --rmg-multi \
+            -v "${VOLUME}:/volume1" \
+            -v "${DATA}:/data" \
+            -- \
+            -rename \
+            --db TheTVDB \
+            --action test \
+            --format "/volume1/filebot-output/{n.colon(' - ')}/{episode.special ? 'Specials' : 'Season '+s.pad(2)}/{n.colon(' - ')} - {episode.special ? 'S00E'+special.pad(2) : s00e00} - {t.replaceAll(/[`´‘’ʻ]/, /'/).replaceAll(/[!?.]+$/).replacePart(', Part $1')}{'.'+lang}" \
+            -non-strict \
+            -r \
+            /volume1 \
+            "$@"
