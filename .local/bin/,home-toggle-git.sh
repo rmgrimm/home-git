b46dcd455957c91e58edece61a23225d56333969
Robert Grimm <rmgrimm@users.noreply.github.com>
2021-03-06 04:35:00 -0600

    Fix typo in home-toggle-git script

--- .local/bin/,home-toggle-git.sh
+++ .local/bin/,home-toggle-git.sh
@@ -7,7 +7,7 @@ if [ -d "$HOME_VCS_STOWED" ]
 then
   mv "$HOME_VCS_STOWED" "$HOME_VCS_NORMAL" || exit 1
   echo "Home directory version control activated"
-elsif [ -d "$HOME_VCS_NORMAL" ]
+elif [ -d "$HOME_VCS_NORMAL" ]
 then
   mv "$HOME_VCS_NORMAL" "$HOME_VCS_STOWED" || exit 1
   echo "Home directory version control deactivated"
