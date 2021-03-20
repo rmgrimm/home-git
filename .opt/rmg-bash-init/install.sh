3dd8b62fc8a60b8a50f1ee055fed1c2de038cfd1
Robert Grimm <rmgrimm@users.noreply.github.com>
2021-03-20 00:33:02 -0500

    Minor clean up

--- .opt/rmg-bash-init/install.sh
+++ .opt/rmg-bash-init/install.sh
@@ -20,8 +20,8 @@ then
   # Ensure .profile is set to source the script that will run each part
   grep -q \
     -F '. "$HOME/.opt/rmg-bash-init/source-profile-parts.sh"' \
-    "$HOME/.bashrc" || \
-  cat >> "$HOME/.bashrc" << 'END_OF_INSERT'
+    "$HOME/.profile" || \
+  cat >> "$HOME/.profile" << 'END_OF_INSERT'
 
 # Run custom profile parts
 [ -r "$HOME/.opt/rmg-bash-init/source-profile-parts.sh" ] && \
