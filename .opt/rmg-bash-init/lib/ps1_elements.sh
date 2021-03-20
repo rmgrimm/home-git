32f1e9f4c5188225e740549a8b04a15d04c528b5
Robert Grimm <rmgrimm@users.noreply.github.com>
2021-03-20 01:19:22 -0500

    Don't bother with hostname in prompt; it's always localhost

--- .opt/rmg-bash-init/lib/ps1_elements.sh
+++ .opt/rmg-bash-init/lib/ps1_elements.sh
@@ -1,6 +1,6 @@
 PS1Time="\t"
 PS1PathShort="\w"
-PS1User="\u@\h"
+PS1User="\u@crouton"
 PS1NewLine="\n"
 PS1ContainerIndicator='${debian_chroot:+($debian_chroot)}'
 if [ -f "/run/.containerenv" ] && [ -f "/run/.toolboxenv" ]; then
