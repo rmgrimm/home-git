f4a8fd0d41ce9b0aa908aa710acfd5449ee88613
Robert Grimm <rmgrimm@users.noreply.github.com>
2021-03-20 01:21:17 -0500

    Rearrange container indicator in prompt

--- .opt/rmg-bash-init/lib/ps1_color.sh
+++ .opt/rmg-bash-init/lib/ps1_color.sh
@@ -16,8 +16,7 @@ elif [ -e "/usr/share/git-core/contrib/completion/git-prompt.sh" ]; then
   HaveGit=1
 fi
 
-PS1='${debian_chroot:+($debian_chroot)}'
-PS1=$PS1$PSColorPre$IBlack$PSColorPost$PS1Time$PSColorPre$Color_Off$PSColorPost
+PS1=$PSColorPre$IBlack$PSColorPost$PS1Time$PSColorPre$Color_Off$PSColorPost
 if [ $HaveGit ]; then
   PS1=$PS1'$(git rev-parse --top-level >/dev/null 2>&1 ;'
   PS1=$PS1'if [ $? -eq 0 ]; then'
@@ -26,19 +25,21 @@ if [ $HaveGit ]; then
   PS1=$PS1'    echo "'$PSColorPre$Green$PSColorPost'"$(__git_ps1 " (%s)");'
   PS1=$PS1'  else'
   PS1=$PS1'    echo "'$PSColorPre$IRed$PSColorPost'"$(__git_ps1 " (%s)");'
-  PS1=$PS1'  fi) '$PS1ColorUser$PS1NewLine
-  if [ -n "$PS1ToolboxIndicator" ]; then
-      PS1=$PS1$PSColorPre$Purple$PSColorPost$PS1ToolboxIndicator
+  PS1=$PS1'  fi) '$PS1ColorUser
+  if [ -n "$PS1ContainerIndicator" ]; then
+      PS1=$PS1$PSColorPre$Purple$PSColorPost$PS1ContainerIndicator
   fi
+  PS1=$PS1$PS1NewLine
   PS1=$PS1$PSColorPre$BYellow$PSColorPost$PS1PathShort
   PS1=$PS1$PSColorPre$Color_Off$PSColorPost'\$ ";'
   PS1=$PS1'else'
   PS1=$PS1'  echo "'
 fi
-PS1=$PS1' '$PS1ColorUser$PS1NewLine
-if [ -n "$PS1ToolboxIndicator" ]; then
-    PS1=$PS1$PSColorPre$Purple$PSColorPost$PS1ToolboxIndicator
+PS1=$PS1' '$PS1ColorUser
+if [ -n "$PS1ContainerIndicator" ]; then
+    PS1=$PS1$PSColorPre$Purple$PSColorPost$PS1ContainerIndicator
 fi
+PS1=$PS1$PS1NewLine
 PS1=$PS1$PSColorPre$Yellow$PSColorPost$PS1PathShort
 PS1=$PS1$PSColorPre$Color_Off$PSColorPost'\$ '
 if [ $HaveGit ]; then
--- .opt/rmg-bash-init/lib/ps1_nocolor.sh
+++ .opt/rmg-bash-init/lib/ps1_nocolor.sh
@@ -1,6 +1,7 @@
 . "$HOME/.opt/rmg-bash-init/lib/ps1_elements.sh"
 
-PS1=$PS1NewLine'${debian_chroot:+($debian_chroot)}'
-PS1=$PS1$PS1Time' '$PS1User$PS1NewLine
+PS1=$PS1NewLine
+PS1=$PS1$PS1Time' '$PS1User
 PS1=$PS1$PS1ToolboxIndicator
+PS1=$PS1$PS1NewLine
 PS1=$PS1$PS1PathShort'\$ '
